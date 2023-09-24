8031c807c03107b5b6d829054945696817cebef4
Xqhare <xqhare@web.de>
2023-09-24 16:14:16 +0200

    added readout of current dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::path::PathBuf;
+
+
+
+fn current_dir() -> std::io::Result<PathBuf> {
+    let path = env::current_dir()?;
+    println!("The current directory is {}", path.display());
+    Ok(path)
+}
 fn main() {
     println!("Hello, world!");
+    let _ = current_dir();
 }
