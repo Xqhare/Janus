856898dc84cd74537ddb053ea7f01b71a6af3b0e
Xqhare <xqhare@web.de>
2023-09-29 13:27:28 +0200

    added rename.rs

--- src/mkdir.rs
+++ src/mkdir.rs
@@ -1,5 +1,8 @@
 use std::{path::Path, fs};
 
+// I think I'm starting to understand!
+// the <> after the function name is te type of the input! AsRef uses any defined type I guess?
+// Also wrapping stuff again! - wrapping is fun, call me a wrapeist!
 pub fn create_dir<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
     fs::create_dir_all(path)?;
     Ok(())
